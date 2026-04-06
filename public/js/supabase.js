60167f0c564ecd9faa668b98921db1c05b43ce56
catarinalopes7 <catarinalusitanadela@live.com.pt>
2026-04-06 02:48:48 +0200

    Create supabase.js

---
+++ public/js/supabase.js
@@ -0,0 +1,6 @@
+<script src="[cdn.jsdelivr.net](https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2)"></script>
+<script>
+  const supabaseUrl = "[seu-projeto.supabase.co](https://SEU-PROJETO.supabase.co)";
+  const supabaseKey = "SUA-ANON-KEY";
+  window.supabase = supabase.createClient(supabaseUrl, supabaseKey);
+</script>
